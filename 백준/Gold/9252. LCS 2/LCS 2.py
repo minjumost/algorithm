d1349afc74426d8d4be1d8379dd87f807a89c89c
최민주 <95904729+minjumost@users.noreply.github.com>
2024-03-11 22:42:33 +0900

    [Gold IV] Title: LCS 2, Time: 572 ms, Memory: 56708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,36 +1,37 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
-A = list(input())
-A.pop()
-B = list(input())
-B.pop()
-DP = [[0 for j in range(len(B) + 1)] for i in range(len(A) + 1)]
-Path = []
-for i in range(1, len(A) + 1):
-    for j in range(1, len(B) + 1):
-        if A[i - 1] == B[j - 1]:
-            DP[i][j] = DP[i - 1][j - 1] + 1
+
+a = list(input().rstrip("\n"))
+b = list(input().rstrip("\n"))
+dp = [[0 for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+path = []
+
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
-            DP[i][j] = max(DP[i - 1][j], DP[i][j - 1])
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-print(DP[len(A)][len(B)])
 
-def getText(r, c):
+def getLcs(r, c):
     if r == 0 or c == 0:
         return
-    if A[r - 1] == B[c - 1]: 
-        Path.append(A[r - 1])
-        getText(r - 1, c - 1)
-    else: 
-        if DP[r - 1][c] > DP[r][c - 1]:
-            getText(r - 1, c)
+
+    if a[r-1] == b[c-1]:
+        path.append(a[r-1])
+        getLcs(r-1,c-1)
+
+    else:
+        if dp[r-1][c] > dp[r][c-1]:
+            getLcs(r-1,c)
         else:
-            getText(r, c - 1)
+            getLcs(r, c-1)
 
 
-getText(len(A), len(B))
+getLcs(len(a), len(b))
 
-for i in range(len(Path) - 1, -1, -1):
-    print(Path.pop(i), end='')
-print()
+print(dp[-1][-1])
+for i in range(len(path)-1, -1, -1):
+    print(path.pop(i), end = '')
