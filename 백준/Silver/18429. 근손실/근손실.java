ebe81b0e94e448f1826aa83a7e6918e88cbef86d
최민주 <95904729+minjumost@users.noreply.github.com>
2024-08-13 15:31:04 +0900

    [Silver III] Title: 근손실, Time: 144 ms, Memory: 14844 KB -BaekjoonHub

---
+++ 백준/Silver/18429. 근손실/근손실.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer stringTokenizer;
+
+	static int n;
+	static int k;
+	static int[] kit;
+	static boolean[] visited;
+	static int answer = 0;
+
+	public static void main(String[] args) throws IOException {
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		n = Integer.parseInt(stringTokenizer.nextToken());
+		k = Integer.parseInt(stringTokenizer.nextToken());
+
+		kit = new int[n];
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		for (int i = 0; i < n; i++) {
+			kit[i] = Integer.parseInt(stringTokenizer.nextToken());
+		}
+
+		visited = new boolean[n + 1];
+		permutation(0, new int[n]);
+		System.out.println(answer);
+	}
+
+	static void permutation(int length, int[] kits) {
+		if (length >= n) {
+			int strength = 500;
+			for (int kit : kits) {
+				strength += (kit - k);
+				if (strength < 500) {
+					return;
+				}
+			}
+
+			answer += 1;
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (visited[i]) {
+				continue;
+			}
+
+			visited[i] = true;
+			kits[length] = kit[i - 1];
+			permutation(length + 1, kits);
+			visited[i] = false;
+		}
+	}
+
+}
