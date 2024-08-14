15387a7bb0e927d62dd4ec1c5fbc26c18035cacc
최민주 <95904729+minjumost@users.noreply.github.com>
2024-08-14 16:57:27 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer stringTokenizer;
+	static StringBuilder stringBuilder;
+	static int N;
+	static int M;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		stringBuilder = new StringBuilder();
+		N = Integer.parseInt(stringTokenizer.nextToken());
+		M = Integer.parseInt(stringTokenizer.nextToken());
+		visited = new boolean[N + 1];
+
+		permutation(0, new int[M]);
+	}
+
+	static void permutation(int depth, int[] temp) {
+		if (depth >= M) {
+			for (int num : temp) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = depth == 0 ? 1 : temp[depth - 1] + 1; i <= N; i++) {
+			if (visited[i]) {
+				continue;
+			}
+
+			visited[i] = true;
+			temp[depth] = i;
+			permutation(depth + 1, temp);
+			visited[i] = false;
+		}
+	}
+
+}
